2dedcd575832a851981ee799177d02ff3a433084
sluongo718 <sluongo718@yahoo.com>
2021-09-04 02:11:37 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
-// Code MovieReviews Here
+
+import React from 'react';
+
+const MovieReviews = ({reviews}) => {
+   
+  return (
+  
+    <ul className='review-list'>
+      {reviews.map(review => 
+        <li key={Math.random()}>
+          <h1 className='review'>{review.display_title}</h1>
+        </li>  
+      )}
+    </ul>
+  )
+}
+
+export default MovieReviews;
